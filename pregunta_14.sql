5b958362ea449e60fa8cdec9b50d435899c5730a
Yezenia Restrepo <125924954+YezeniaR@users.noreply.github.com>
2023-06-10 10:46:57 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,8 +43,8 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT tbl1.K0, AVG(tbl2.c21) AS avg_c21
-FROM tbl1
-JOIN tbl2 ON tbl1.K1 = tbl2.K1
-WHERE tbl1.c13 > 400
-GROUP BY tbl1.K0;
+SELECT K0, avg(c21)
+FROM tbl2 JOIN tbl1
+ON tbl1.K1 = tbl2.K1
+WHERE c13 > 400
+GROUP BY K0
