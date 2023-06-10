35c1dd8630ae8f9fc99e1659c680357f2f7ba72d
Yezenia Restrepo <125924954+YezeniaR@users.noreply.github.com>
2023-06-10 10:47:20 -0500

    Update pregunta_13.sql

--- pregunta_13.sql
+++ pregunta_13.sql
@@ -43,7 +43,6 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, AVG(c12) AS avg_c12
-FROM tbl1
+SELECT K0, avg(c12) FROM tbl1
 WHERE c13 > 400
-GROUP BY K0;
+GROUP BY K0
