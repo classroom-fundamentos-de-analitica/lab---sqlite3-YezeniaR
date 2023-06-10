a5e9af99d730f155bd15fad81047b8edb1f259d9
Yezenia Restrepo <125924954+YezeniaR@users.noreply.github.com>
2023-06-10 10:37:57 -0500

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,6 +41,6 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT YEAR(c23) AS YEAR, AVG(c21) AS avg_c21
+SELECT strftime('%Y', c23), avg(c21) 
 FROM tbl2
-GROUP BY YEAR(c23);
+GROUP BY strftime('%Y', c23)
